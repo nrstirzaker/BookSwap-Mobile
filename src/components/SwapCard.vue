a48f3fe560b094e4820c0147f5664daa2818c59a
alexraju01 <alex.raju1010@gmail.com>
2026-06-10 15:40:07 +0100

    Created SwapCard also added mock data object to test the component

---
+++ src/components/SwapCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative cursor-pointer overflow-hidden rounded-2xl">
+    <q-img :src="image" :alt="title" :ratio="1.5" class="bg-gray-200" />
+
+    <div class="absolute bottom-0 left-0 w-full p-3 bg-linear-to-t from-black/70 to-transparent">
+      <div class="text-white font-semibold text-sm truncate">
+        {{ title }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  image: string;
+}>();
+</script>
