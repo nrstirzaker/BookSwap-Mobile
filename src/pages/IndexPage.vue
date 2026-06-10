162bb1339d404c3a2b7f75b6dfdf99a37ec73855
alexraju01 <alex.raju1010@gmail.com>
2026-06-10 11:06:05 +0100

    Rounded the search box and added background image for the page from unsplash

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-page class="bg-grey-1">
-    <div class="q-pa-md q-gutter-sm"></div>
-
+  <q-page
+    class="bg-cover bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1506744038136-46273834b3fb')]"
+  >
     <div class="q-pa-md row items-center justify-between">
       <q-avatar>
         <img src="https://cdn.quasar.dev/img/avatar.png" />
