8263737d08546e3d7e3bdc0d63d88c165a093be2
alexraju01 <alex.raju1010@gmail.com>
2026-06-17 14:23:18 +0100

    Created a MainHeader component which is the user avatar to make the MainLayout even more clean

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-header flat class="bg-transparent text-gray-800">
+    <div class="q-pa-md row items-center justify-between text-black">
+      <q-btn round flat icon="menu" @click="emit('toggle-menu')" />
+
+      <q-avatar class="cursor-pointer">
+        <img :src="avatarUrl" alt="User Profile" />
+
+        <q-menu auto-close>
+          <q-list class="min-w-30">
+            <q-item clickable to="/profile">
+              <q-item-section>My Profile</q-item-section>
+            </q-item>
+            <q-item clickable>
+              <q-item-section class="text-negative">Logout</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-avatar>
+    </div>
+  </q-header>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'toggle-menu'): void;
+}>();
+
+const avatarUrl = 'https://cdn.quasar.dev/img/avatar.png';
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,15 +3,7 @@
     view="lHh Lpr lFf"
     class="min-h-screen bg-cover bg-center bg-no-repeat bg-[url('https://images.unsplash.com/photo-1506744038136-46273834b3fb')]"
   >
-    <q-header flat class="bg-transparent text-gray-800">
-      <div class="q-pa-md row items-center justify-between text-black">
-        <q-btn round flat icon="menu" @click="toggleLeftDrawer" />
-
-        <q-avatar>
-          <img src="https://cdn.quasar.dev/img/avatar.png" />
-        </q-avatar>
-      </div>
-    </q-header>
+    <MainHeader @toggle-menu="toggleLeftDrawer" />
 
     <NavigationDrawer v-model="leftDrawerOpen" />
 
@@ -23,6 +15,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import MainHeader from 'components/MainHeader.vue';
 import NavigationDrawer from 'components/NavigationDrawer.vue';
 
 const leftDrawerOpen = ref(false);
